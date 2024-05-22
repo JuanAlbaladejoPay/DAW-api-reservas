dee344326afbb373150cd5fb12e747d3f51ecb60
Juan <1883216@alu.murciaeduca.es>
2024-05-22 19:50:19 +0200

    feat: create EmailService

--- src/Controller/Security/RegistrationController.php
+++ src/Controller/Security/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Security;
 // He añadio \Security
 
 use App\Repository\UserRepository;
+use App\Service\EmailService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
@@ -24,14 +25,16 @@ use Symfony\Component\Mime\Email;
 #[Route('/api', name: 'api_')]
 class RegistrationController extends AbstractController {
   private $JWTManager;
+  private EmailService $emailService;
 
-  public function __construct(JWTTokenManagerInterface $JWTManager) { // Añadimos el servicio de verificación de email al constructor
+  public function __construct(JWTTokenManagerInterface $JWTManager, EmailService $emailService) { // Añadimos el servicio de verificación de email al constructor
     $this->JWTManager = $JWTManager;
+    $this->emailService = $emailService;
   }
 
   #[Route('/register', name: 'register', methods: 'POST')]
   public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, VerifyEmailHelperInterface $verifyEmailHelper): JsonResponse {
-    $dataBody = json_decode($request->getContent(), true); // Obtenemos los datos de la petición en un array asociativo (al pasar TRUE como parámetro)
+    $dataBody = json_decode($request->getContent(), true);
 
     // Comprobamos que los campos requeridos están en la petición
     $missingFieldMessage = $this->verifyRequiredFields($dataBody);
@@ -91,32 +94,9 @@ class RegistrationController extends AbstractController {
 
 
     $token = $this->JWTManager->create($newUser);
-    // TODO: Falta añadir la url de verificación al email cuando lo envíamos!!
-    // --> Enviar EMAIL (lo dejo comentado para descomentar solo cuando haga falta enviar un email de verdad)
-    /* try {
-      $transport = Transport::fromDsn($_ENV['MAILER_DSN']); // Tenemos que poner en .env esta variable: MAILER_DSN=smtp://letsmove.murcia@gmail.com:PASSWORD@smtp.gmail.com:587 (Password está en el drive)
-      $mailer = new Mailer($transport);
 
-      // Crear el correo electrónico
-      $email = (new Email())
-        ->from('letsmove.murcia@gmail.com')
-        ->to($newUser->getEmail())
-        ->subject('¡Bienvenido a LetsMove!')
-        ->html("
-                <h1>¡Bienvenido a LetsMove!</h1>
-                <p>¡Gracias por registrarte en nuestro sitio!</p>
-                <p>Estamos emocionados de tenerte con nosotros. Aquí podrás encontrar las mejores actividades deportivas en tu área.</p>
-                <p>Si tienes alguna pregunta, no dudes en contactarnos.</p>
-                <p>¡Disfruta de LetsMove!</p>
-                <p>Verifica tu cuenta en: {$urlVerificacion}</p>
-            ");
-
-      // Enviar el correo electrónico
-      $mailer->send($email);
-
-    } catch (\Exception $e) {
-      return $this->json(['error' => "Ha ocurrido un error al enviar el email $e"], 500);
-    }*/
+    // TODO falta descomentar esto para enviar los emails
+//    $this->emailService->sendRegistrationEmail($newUser->getEmail(), $urlVerificacion);
 
     return $this->json(['ok' => 'Te has registrado correctamente', 'message' => 'Se ha enviado un email de verificacion a tu correo']);
   }
